dcccbd0d9fc6f4464347c39a07f16806165e6872
奋斗的土豆 <2628410151@qq.com>
2019-11-04 17:20:17 +0800

    黄勃树啦啦啦

--- src/main/webapp/js/拖轮调度.js
+++ src/main/webapp/js/拖轮调度.js
@@ -1,44 +1,44 @@
 /**
- * 船舶维修计费
+ * 库存统计
+ * @type {((...tabularData: any[]) => void) | string | []}
  */
 layui.use('table', function() {
     var table = layui.table;
     form = layui.form
     /**
-     * 方法级渲染
+     * 数据表格
      */
     table.render({
+        id: 'test',
         method: 'post',
         elem: '#test',
         contentType: 'application/json',
         dataType: 'json',
         loading: 'true',
-        url: user.defaultUrl + myurl.weixiuList,
+        url: user.defaultUrl + myurl.tuolunList,
         toolbar: '#toolbarDemo',
         title: '用户数据表',
         totalRow: true,
         initSort: {
             field: 'id' //排序字段，对应 cols 设定的各字段名
-            , type: 'asc' //排序方式  asc: 升序、desc: 降序、null: 默认排序
+            ,type: 'asc' //排序方式  asc: 升序、desc: 降序、null: 默认排序
         },
         text: {
             none: '小小数据去哪了？'
         },
         autoSort: 'true',
         // skin: 'nob',//是否显示行列渲染
-        even: true,
+        // even: true,
         size: 'lg',
-        id: 'test',
         page: true,
         limit: 8,
         limits: [7, 10, 15],
         cols: [[
-            {field: 'id',title: 'ID', sort: true, align: 'center', fixed: true}
-            , {field: 'boat', title: '机器名', align: 'center'}
-            , {field: 'people', title: '负责人', sort: true, align: 'center'}
-            , {field: 'status', title: '是否已维修', align: 'center', templet:'#manager_status'}
-            , {field: 'dataline', title: '维修时间', minWidth: 100, align: 'center'}
-            , {field: 'money', title: '维修金额', sort: true, align: 'center'}
+            {field:'id', title: 'ID', width:'10%', sort: true, align: 'center', fixed: true}
+            ,{field:'boat',title: '船只名', align: 'center'}
+            ,{field:'people',title: '负责员工', align: 'center'}
+            ,{field:'dataline', title: '时间', align: 'center'}
+            ,{field:'status', title: '拖轮状态', align: 'center', templet:'#manager_status'}
         ]],
         /**
          * 回调函数
@@ -48,6 +48,6 @@ layui.use('table', function() {
          */
         done: function(res, curr, count) {
             console.log(res);
-        }
+        },
     });
 });
