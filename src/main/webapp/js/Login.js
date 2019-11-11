514c955be4494a85171a9d8fe73b7213e58e8ea8
奋斗的土豆 <2628410151@qq.com>
2019-11-11 11:48:45 +0800

    黄勃树啦啦啦

--- src/main/webapp/js/Login.js
+++ src/main/webapp/js/Login.js
@@ -3,8 +3,9 @@ function Login(){
     var username = $("#username").val();
     var s = req.post(myurl.login,{pwd: password, username: username} , false);
     if(s.ret == 200){
+        alert("欢迎回来!")
         window.open("首页.html")
     }else {
-        alert("账号密码错误")
+        alert("账号密码错误!")
     }
 }
